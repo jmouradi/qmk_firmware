5def13b3a302ad9638b5729a01b41b5f2a0aa101
James Mouradian <james.a.mouradian@gmail.com>
2022-12-13 23:21:04 -0800

    Adds vim save key. Also deletes oryx mode.

--- keyboards/moonlander/keymaps/coming_out_layout/keymap.c
+++ keyboards/moonlander/keymaps/coming_out_layout/keymap.c
@@ -50,17 +50,16 @@
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
+  VIM_SAVE,
 };
-
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_ESCAPE,                                      TG(2),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_HOME,                                        TOGGLE_LAYER_COLOR,KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,      
-    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_END,                                                                         KC_TRANSPARENT,          KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_END,                                                                         VIM_SAVE,          KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_CAPSLOCK,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
-    KC_GRAVE,       KC_HOME,        KC_END,         KC_LEFT,        KC_RIGHT,       KC_LCTRL,                                                                                                       KC_RGUI,        KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    TG(1),
+    KC_GRAVE,       VIM_SAVE,       KC_END,         KC_LEFT,        KC_RIGHT,       KC_LCTRL,                                                                                                       KC_RGUI,        KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    TG(1),
     KC_BSPACE,      KC_DELETE,      KC_ESCAPE,                      KC_LALT,        KC_ENTER,       KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
@@ -139,8 +138,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(1);
       }
       return false;
+    case VIM_SAVE:
+      if (record->event.pressed) {
+          SEND_STRING(SS_TAP(X_ESCAPE)SS_TAP(X_ESCAPE)SS_TAP(X_ESCAPE)":w"SS_TAP(X_ENTER));
+      }
+      return false; // Custom keycode means nothing; do not send.
   }
+
   return true;
 }
-
 
